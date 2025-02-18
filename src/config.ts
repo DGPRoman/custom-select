426af8b23d93db39b7442b828cc645885d8d46d2
roman <lostspace666@gmail.com>
2025-02-18 19:14:47 +0200

    add note in config file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+/**
+ * API configuration. The API key is included here for quick setup and testing of the project.
+ * Note: For production, this configuration should be moved to an environment file.
+ */
 export const config = {
     API_URL: "https://parseapi.back4app.com",
     API_ID: "zsSkPsDYTc2hmphLjjs9hz2Q3EXmnSxUyXnouj1I",
